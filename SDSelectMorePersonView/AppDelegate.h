cb70a8cf0f93afbb01aab7793252f2b473d88732
吕东 <435574793@qq.com>
2017-02-09 17:21:08 +0800

    部门和角色,人员可以多选控件

---
+++ SDSelectMorePersonView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDSelectMorePersonView
+//
+//  Created by apple on 2017/2/9.
+//  Copyright © 2017年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDSelectMorePersonView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDSelectMorePersonView
+//
+//  Created by apple on 2017/2/9.
+//  Copyright © 2017年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
